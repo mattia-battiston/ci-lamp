7588eec6e3896fa085feda2a04853c8a8461157a
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-06-19 21:19:52 +0000

    working on "ALARM ON" button

--- src/com/cilamp/command/AlarmOnCommand.java
+++ src/com/cilamp/command/AlarmOnCommand.java
@@ -1,9 +1,11 @@
 package com.cilamp.command;
 
+import com.cilamp.serial.SerialPortInterface;
+
 public class AlarmOnCommand {
 
   public void execute() {
-
+    String command = "A1";
+    SerialPortInterface.getInstance().sendCommand(command);
   }
-
 }
