e93aabb4f43de749131dd2ddc11cb57e6edaaa1e
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-06-21 11:38:37 +0000

    refactoring

--- src/com/cilamp/gui/app/CILampGui.java
+++ src/com/cilamp/gui/app/CILampGui.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import com.cilamp.command.AlarmOnCommand;
+import com.cilamp.service.command.AlarmOnCommand;
 
 public class CILampGui {
 
