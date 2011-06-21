1e325501974bcfb2bdc9b874b6b4062a9304b7d6
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-06-21 21:15:02 +0000

    covering code

--- test/com/cilamp/gui/app/CILampGuiPresenterTest.java
+++ test/com/cilamp/gui/app/CILampGuiPresenterTest.java
@@ -1,14 +1,65 @@
 package com.cilamp.gui.app;
 
-import static junit.framework.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.awt.Button;
+import java.awt.event.ActionListener;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.cilamp.service.services.LampService;
 
 public class CILampGuiPresenterTest {
 
+  private CILampGuiPresenter presenter;
+
+  @Mock
+  private CILampGuiPresenter.View view;
+
+  @Mock
+  private Button alarmOnButton;
+
+  @Mock
+  private LampService lampService;
+
+  @Before
+  public void before() {
+    MockitoAnnotations.initMocks(this);
+
+    mockView();
+    presenter = new CILampGuiPresenter(view);
+    presenter.setLampService(lampService);
+  }
+
   @Test
-  public void test() {
-    fail("TODO");
+  public void alarmOnCallsService() {
+    ActionListener alarmOnListener = getActionListenerForButton(alarmOnButton);
+    alarmOnListener.actionPerformed(null);
+
+    verify(lampService).turnAlarmOn();
+  }
+
+  @Test
+  public void showShowsTheView() {
+    presenter.show();
+
+    verify(view).show();
+  }
+
+  private ActionListener getActionListenerForButton(Button button) {
+    ArgumentCaptor<ActionListener> actionListenerCaptor = ArgumentCaptor
+        .forClass(ActionListener.class);
+    verify(button).addActionListener(actionListenerCaptor.capture());
+    return actionListenerCaptor.getValue();
+  }
+
+  private void mockView() {
+    when(view.getAlarmOnButton()).thenReturn(alarmOnButton);
   }
 
 }
