c93d6419703c80055739acc56687d6eb78231638
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-06-22 12:20:00 +0000

    refactoring

--- src/com/cilamp/serial/SerialPortInterface.java
+++ src/com/cilamp/serial/SerialPortInterface.java
@@ -1,14 +1,10 @@
 package com.cilamp.serial;
 
-import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
-import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
-import gnu.io.UnsupportedCommOperationException;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Enumeration;
 
 public class SerialPortInterface {
 
@@ -22,48 +18,40 @@ public class SerialPortInterface {
   }
 
   private SerialPortInterface() {
-    initialize();
+    try {
+      initialize();
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
   }
 
   // TODO get this from properties file
   private static final String SERIAL_PORT_NAME = "COM1";
   private OutputStream serialPortOutputStream;
 
-  public void initialize() {
-    CommPortIdentifier commPortIdentifier = getSerialPort();
+  public void initialize() throws Exception {
+    CommPortIdentifier commPortIdentifier = CommPortIdentifier
+        .getPortIdentifier(SERIAL_PORT_NAME);
+
+    if (commPortIdentifier == null) {
+      throw new RuntimeException("Serial port " + SERIAL_PORT_NAME
+          + " is not available");
+    }
+    if (commPortIdentifier.getPortType() != CommPortIdentifier.PORT_SERIAL) {
+      throw new RuntimeException(SERIAL_PORT_NAME + " is not a serial port");
+    }
+    if (commPortIdentifier.isCurrentlyOwned()) {
+      throw new RuntimeException("Port is currently in use");
+    }
+
     System.out.println("Found port COM1");
 
-    CommPort genericPort;
-    try {
-      genericPort = commPortIdentifier.open("MattiaTestApp", 2000);
-      SerialPort serialPort = (SerialPort) genericPort;
-      // TODO understand these parameters
-      serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,
-          SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-      serialPortOutputStream = serialPort.getOutputStream();
-
-    } catch (PortInUseException e) {
-      e.printStackTrace();
-    } catch (UnsupportedCommOperationException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @SuppressWarnings("rawtypes")
-  private CommPortIdentifier getSerialPort() {
-    Enumeration portList = CommPortIdentifier.getPortIdentifiers();
-
-    while (portList.hasMoreElements()) {
-      CommPortIdentifier port = (CommPortIdentifier) portList.nextElement();
-      if (port.getPortType() == CommPortIdentifier.PORT_SERIAL
-          && port.getName().equals(SERIAL_PORT_NAME))
-        return port;
-    }
-
-    throw new RuntimeException("Serial port " + SERIAL_PORT_NAME
-        + " is not available");
+    SerialPort serialPort = (SerialPort) commPortIdentifier
+        .open("CILamp", 2000);
+    // TODO understand these parameters
+    serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,
+        SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+    serialPortOutputStream = serialPort.getOutputStream();
   }
 
   public void sendCommand(String command) {
