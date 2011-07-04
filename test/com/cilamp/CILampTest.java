def57adabd6e0f12ff4db96713ac5de49063d10a
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-04 20:16:09 +0000

    implement an event bus to dispatch events between threads and gui

--- test/com/cilamp/CILampTest.java
+++ test/com/cilamp/CILampTest.java
@@ -9,9 +9,13 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.cilamp.event.LampTurnedOffEvent;
+import com.cilamp.event.LampTurnedOnEvent;
 import com.cilamp.event.base.EventBus;
 import com.cilamp.gui.app.CILampGui;
 import com.cilamp.gui.app.CILampGuiPresenter;
+import com.cilamp.gui.app.LampTurnedOffHandler;
+import com.cilamp.gui.app.LampTurnedOnHandler;
 import com.cilamp.gui.tray.CILampTrayService;
 
 public class CILampTest {
@@ -55,6 +59,22 @@ public class CILampTest {
     ciLamp.initializeApplication();
 
     verify(ciLampGui).initialize(any(CILampGui.class), eq(bus));
+  }
+
+  @Test
+  public void setHandlerForLampTurnedOnEvent() {
+    ciLamp.initializeApplication();
+
+    verify(bus).addHandler(eq(LampTurnedOnEvent.TYPE),
+        any(LampTurnedOnHandler.class));
+  }
+
+  @Test
+  public void setHandlerForLampTurnedOffEvent() {
+    ciLamp.initializeApplication();
+
+    verify(bus).addHandler(eq(LampTurnedOffEvent.TYPE),
+        any(LampTurnedOffHandler.class));
   }
 
 }
