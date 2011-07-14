1a7f17fd83e03a669fad31ca3d3537f2fd64df1b
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-14 12:11:58 +0000

    cover some code

--- test/com/cilamp/gui/app/LampTurnedOffHandlerTest.java
+++ test/com/cilamp/gui/app/LampTurnedOffHandlerTest.java
@@ -1,14 +1,53 @@
 package com.cilamp.gui.app;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.awt.Button;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class LampTurnedOffHandlerTest {
 
+  private LampTurnedOffHandler handler;
+
+  @Mock
+  private CILampGuiPresenter.View view;
+
+  @Mock
+  private Button alarmOnButton;
+
+  @Mock
+  private Button alarmOffButton;
+
+  @Before
+  public void before() {
+    MockitoAnnotations.initMocks(this);
+
+    handler = new LampTurnedOffHandler(view);
+    mockView();
+  }
+
   @Test
-  public void test() {
-    fail("TODO");
+  public void enableAlarmOnButtonWhenLampTurnsOff() {
+    handler.onLampTurnedOff(null);
+
+    verify(alarmOnButton).setEnabled(true);
+  }
+
+  @Test
+  public void disableAlarmOffButtonWhenLampTurnsOff() {
+    handler.onLampTurnedOff(null);
+
+    verify(alarmOffButton).setEnabled(false);
+  }
+
+  private void mockView() {
+    when(view.getAlarmOnButton()).thenReturn(alarmOnButton);
+    when(view.getAlarmOffButton()).thenReturn(alarmOffButton);
   }
 
 }
