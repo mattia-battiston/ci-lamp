249421303474a572c470281f053e1ed4cdce55f4
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-17 17:43:34 +0000

    show build information after it has been loaded

--- trunk/src/com/cilamp/service/services/DomRetriever.java
+++ trunk/src/com/cilamp/service/services/DomRetriever.java
@@ -1,12 +1,34 @@
 package com.cilamp.service.services;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 
 public class DomRetriever {
 
-  public Document getDom(String url) {
-    // TODO Auto-generated method stub
-    return null;
+  private SAXReader reader = new SAXReader();
+
+  public Document getDom(String endpoint) {
+    try {
+      return doGetDom(endpoint);
+    } catch (Exception e) {
+      throw new RuntimeException("Error reading document at " + endpoint + ": "
+          + e.getMessage(), e);
+    }
+  }
+
+  private Document doGetDom(String endpoint) throws MalformedURLException,
+      DocumentException {
+    URL url = new URL(endpoint);
+
+    return reader.read(url);
+  }
+
+  public void setReader(SAXReader reader) {
+    this.reader = reader;
   }
 
 }
