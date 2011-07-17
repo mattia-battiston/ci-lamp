874c45dc756bb9748c4149e91ea096ff07f3efe2
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-17 17:11:28 +0000

    show build information after it has been loaded

---
+++ trunk/test/com/cilamp/gui/app/BuildStatusLoaderHandlerTest.java
@@ -0,0 +1,14 @@
+package com.cilamp.gui.app;
+
+import static junit.framework.Assert.fail;
+
+import org.junit.Test;
+
+public class BuildStatusLoaderHandlerTest {
+
+  @Test
+  public void test() {
+    fail("TODO");
+  }
+
+}
