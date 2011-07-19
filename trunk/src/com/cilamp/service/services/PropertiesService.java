019715e8c84abb6a23efa2d8db74a7a05cbf56a7
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-19 12:01:24 +0000

    properties service

---
+++ trunk/src/com/cilamp/service/services/PropertiesService.java
@@ -0,0 +1,32 @@
+package com.cilamp.service.services;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+public class PropertiesService {
+
+  private static Properties properties = new Properties();
+
+  static {
+    initialize();
+  }
+
+  private static void initialize() {
+    URL url = ClassLoader.getSystemResource("cilamp.properties");
+    try {
+      properties.load(url.openStream());
+    } catch (IOException e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  }
+
+  public String getEndpoint() {
+    return properties.getProperty("endpoint");
+  }
+
+  public String getJobName() {
+    return properties.getProperty("jobname");
+  }
+
+}
---
+++ trunk/test/com/cilamp/service/services/PropertiesServiceTest.java
@@ -0,0 +1,27 @@
+package com.cilamp.service.services;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PropertiesServiceTest {
+
+  private PropertiesService propertiesService;
+
+  @Before
+  public void before() {
+    propertiesService = new PropertiesService();
+  }
+
+  @Test
+  public void readEndpoint() {
+    assertNotNull(propertiesService.getEndpoint());
+  }
+
+  @Test
+  public void readJobName() {
+    assertNotNull(propertiesService.getJobName());
+  }
+
+}
