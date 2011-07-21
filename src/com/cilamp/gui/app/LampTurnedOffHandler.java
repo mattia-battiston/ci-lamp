fa216218d2d40a249bf076ebc93b78642c2c6e75
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-21 19:28:32 +0000

    add some log

--- src/com/cilamp/gui/app/LampTurnedOffHandler.java
+++ src/com/cilamp/gui/app/LampTurnedOffHandler.java
@@ -1,9 +1,14 @@
 package com.cilamp.gui.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.cilamp.event.LampTurnedOffEvent;
 import com.cilamp.event.LampTurnedOffEventHandler;
 
 public class LampTurnedOffHandler implements LampTurnedOffEventHandler {
+
+  final Logger log = LoggerFactory.getLogger(LampTurnedOffHandler.class);
 
   private CILampGuiPresenter.View view;
 
@@ -13,6 +18,7 @@ public class LampTurnedOffHandler implements LampTurnedOffEventHandler {
 
   @Override
   public void onLampTurnedOff(LampTurnedOffEvent lampTurnedOffEvent) {
+    log.info("Lamp turned off, handling event");
     view.getAlarmOnButton().setEnabled(true);
     view.getAlarmOffButton().setEnabled(false);
   }
--- src/com/cilamp/gui/app/LampTurnedOnHandler.java
+++ src/com/cilamp/gui/app/LampTurnedOnHandler.java
@@ -1,9 +1,14 @@
 package com.cilamp.gui.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.cilamp.event.LampTurnedOnEvent;
 import com.cilamp.event.LampTurnedOnEventHandler;
 
 public class LampTurnedOnHandler implements LampTurnedOnEventHandler {
+
+  final Logger log = LoggerFactory.getLogger(LampTurnedOnHandler.class);
 
   private CILampGuiPresenter.View view;
 
@@ -13,6 +18,7 @@ public class LampTurnedOnHandler implements LampTurnedOnEventHandler {
 
   @Override
   public void onLampTurnedOn(LampTurnedOnEvent event) {
+    log.info("Lamp turned on, handling event");
     view.getAlarmOnButton().setEnabled(false);
     view.getAlarmOffButton().setEnabled(true);
   }
