9c3c42a7e19f65e320e6d8cbd4492469d6293d72
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-24 15:05:48 +0000

    notify lamp after having checked the build status

--- src/com/cilamp/gui/app/BuildStatusLoadedHandler.java
+++ src/com/cilamp/gui/app/BuildStatusLoadedHandler.java
@@ -9,12 +9,15 @@ import com.cilamp.event.BuildStatusLoadedEvent;
 import com.cilamp.event.BuildStatusLoadedEventHandler;
 import com.cilamp.gui.app.CILampGuiPresenter.View;
 import com.cilamp.model.Build;
+import com.cilamp.service.services.LampService;
 
 public class BuildStatusLoadedHandler implements BuildStatusLoadedEventHandler {
 
   final Logger log = LoggerFactory.getLogger(BuildStatusLoadedHandler.class);
 
   private final View view;
+
+  private LampService lampService;
 
   public BuildStatusLoadedHandler(CILampGuiPresenter.View view) {
     this.view = view;
@@ -29,10 +32,21 @@ public class BuildStatusLoadedHandler implements BuildStatusLoadedEventHandler {
     view.setBuildNumber(build.getNumber());
     view.setBuildUrl(build.getUrl());
     view.setBuildCommitters(toString(build.getCommitters()));
+
+    // TODO this should stay in a different handler; have one to update the
+    // view, and one to update the lamp
+    if ("SUCCESS".equals(build.getStatus()))
+      lampService.turnAlarmOff();
+    else
+      lampService.turnAlarmOn();
   }
 
   private String toString(Set<String> committers) {
     String result = committers.toString();
     return result.substring(1, result.length() - 1);
+  }
+
+  public void setLampService(LampService lampService) {
+    this.lampService = lampService;
   }
 }
