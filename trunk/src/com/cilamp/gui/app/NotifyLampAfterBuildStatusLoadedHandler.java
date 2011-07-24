abab59669d8daca6504199f9d38850ef405cb90a
mattia.battiston@gmail.com <mattia.battiston@gmail.com@5d27d038-6294-ca2c-fbe4-e7047bb244a2>
2011-07-24 15:12:39 +0000

    different handlers to refresh the view and to notify the lamp after build has been checked

---
+++ trunk/src/com/cilamp/gui/app/NotifyLampAfterBuildStatusLoadedHandler.java
@@ -0,0 +1,33 @@
+package com.cilamp.gui.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cilamp.event.BuildStatusLoadedEvent;
+import com.cilamp.event.BuildStatusLoadedEventHandler;
+import com.cilamp.model.Build;
+import com.cilamp.service.services.LampService;
+
+public class NotifyLampAfterBuildStatusLoadedHandler implements
+    BuildStatusLoadedEventHandler {
+
+  final Logger log = LoggerFactory
+      .getLogger(NotifyLampAfterBuildStatusLoadedHandler.class);
+
+  private LampService lampService;
+
+  @Override
+  public void onBuildStatusLoaded(BuildStatusLoadedEvent buildStatusLoadedEvent) {
+    log.info("build loaded, notifying lamp of current status");
+    Build build = buildStatusLoadedEvent.getBuild();
+
+    if ("SUCCESS".equals(build.getStatus()))
+      lampService.turnAlarmOff();
+    else
+      lampService.turnAlarmOn();
+  }
+
+  public void setLampService(LampService lampService) {
+    this.lampService = lampService;
+  }
+}
